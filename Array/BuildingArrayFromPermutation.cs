2964c10f6b9522600c7f8f10f84c00d97686f6c2
Shweta Srivastava <shwetasrivastava2804@gmail.com>
2022-05-11 23:42:01 +0530

    Add project files.

---
+++ Array/BuildingArrayFromPermutation.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+   public class BuildingArrayFromPermutation
+    {
+        public int[] BuildArray(int[] nums)
+        {
+            int[] ans = new int[nums.Length];
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                ans[j] = nums[nums[i]];
+                j++;
+            }
+            return ans;
+        }
+    }
+}
