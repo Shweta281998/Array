a11b49bd23a64ac54003bc3bfa5f6f514a486bef
Shweta Srivastava <shwetasrivastava2804@gmail.com>
2022-05-16 21:05:53 +0530

    new code

---
+++ Array/MatrixDiagonalSum.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Array
+{
+   public class MatrixDiagonalSum
+    {
+        public int DiagonalSum(int[][] mat)
+        {
+            int sum = 0, n = mat.Length;
+            for (int i = 0; i < mat.Length; i++)
+            {
+                sum += mat[i][i] + mat[i][n - i - 1];
+            }
+            return n % 2 == 0 ? sum : sum - mat[n / 2][n / 2];
+        }
+    }
+}
